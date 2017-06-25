54d585b465953e86d98c7bc67b3202f57c6fd547
Rob Saunders <rob@wearebrandnew.com>
2017-06-25 13:19:37 +0800

    improved build script

--- build.sh
+++ build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-DYLIB_FILE=libdd_core.dylib
+DYLIB_FILE=./target/release/libdd_core.dylib
 VST_NAME=DDOverdriveGUI
 
-m -rf ~/Library/Audio/Plug-Ins/VST/$VST_NAME.vst 2> /dev/null
-rm ./target/release/*.d 2> /dev/null
-rm ./target/release/*.rlib 2> /dev/null
-rm ./target/release/*.dylib 2> /dev/null
+rm -rf ~/Library/Audio/Plug-Ins/VST/$VST_NAME.vst 2> /dev/null
+rm -rf $DYLIB_FILE 2> /dev/null
 
 cargo build --release
 
-if [ -f ./target/release/$DYLIB_FILE ]; then
-    vst-bundler $VST_NAME ./target/release/$DYLIB_FILE &&
+if [ -f $DYLIB_FILE ]; then
+    vst-bundler $VST_NAME $DYLIB_FILE &&
     mv -v ./$VST_NAME.vst ~/Library/Audio/Plug-Ins/VST/
 
     du -sh ~/Library/Audio/Plug-Ins/VST/$VST_NAME.vst
